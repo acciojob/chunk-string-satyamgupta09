994d794da01df8c874b8c6961eaab210a7c684bf
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-24 23:15:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 function stringChop(str, size) {
   // Handle edge cases
-  if (str.length === 0 || size <= 0) return [];
+  // if (str.length === 0 || size <= 0) return [];
 
-  let ans = [];
-  let temp = "";
-  let x = 0;
+  // let ans = [];
+  // let temp = "";
+  // let x = 0;
 
-  for (let i = 0; i < str.length; i++) { // Corrected `int` to `let`
-    temp += str[i]; // Add current character to temp
-    x++;
+  // for (let i = 0; i < str.length; i++) { // Corrected `int` to `let`
+  //   temp += str[i]; // Add current character to temp
+  //   x++;
 
-    if (x === size) {
-      ans.push(temp); // Push the completed chunk to ans
-      x = 0; // Reset counter
-      temp = ""; // Reset temp
-    }
-  } 
+  //   if (x === size) {
+  //     ans.push(temp); // Push the completed chunk to ans
+  //     x = 0; // Reset counter
+  //     temp = ""; // Reset temp
+  //   }
+  // } 
 
-  // Add remaining characters as the last chunk
-  if (temp.length > 0) {
-    ans.push(temp);
-  }
+  // // Add remaining characters as the last chunk
+  // if (temp.length > 0) {
+  //   ans.push(temp);
+  // }
 
-  return ans;
+  // return ans;
+	 if (!str) return [];
+	let res=[];
+
+	for(let i=0;i<str.lenght;i+=size){
+		res.push(str.substring(i,i+size));
+	}
+
+	return res;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = parseInt(prompt("Enter Chunk Size."), 10); // Convert size to integer
+// const str = prompt("Enter String.");
+// const size = parseInt(prompt("Enter Chunk Size."), 10); // Convert size to integer
 alert(stringChop(str, size));
